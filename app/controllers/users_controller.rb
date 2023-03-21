a03a18483a57c58f61e99f8ed3d4c9b8d26cff99
Adam Bailey <ab67319@gmail.com>
2023-03-21 16:50:26 -0600

    feat: create functionality for register page
    Co-authored-by: James Taylor <jrtaylor0614@gmail.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,26 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
+    user = User.new(user_params)
+
+    if user.save
+      flash[:notice] = "#{user.email} created"
+      redirect_to user_path(user)
+    else
+      flash[:notice] = "Unable to create new user - #{user.errors.full_messages}"
+      redirect_to register_path
+    end
+  end
+
+  def show
+
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
