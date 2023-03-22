a1c3132aba2dc9ad0f88ce88e6d82d299cdb7f58
Adam Bailey <ab67319@gmail.com>
2023-03-22 12:51:37 -0600

    test: create movies index spec file
    Co-authored-by: James Taylor <jrtaylor0614@gmail.com>

--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -3,13 +3,13 @@ require "rails_helper"
 RSpec.describe "Movies Index Page" do
   scenario "When I visit the discover movies page after clicking Top Movies" do
     it "should return the top movies results" do
-
+      
     end
   end  
 
   scenario "When I visit the discover movies page after clicking Search button" do
     it "should return the first twenty movie results for a given search string" do
-      
+
     end
   end
 end
