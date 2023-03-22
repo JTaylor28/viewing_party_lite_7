2f1f26b6353fc10209eb874246f6d496b733df9d
Adam Bailey <ab67319@gmail.com>
2023-03-22 11:10:56 -0600

    refactor: update user_discover controllers
    Co-authored-by: James Taylor <jrtaylor0614@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "home#index"
 
   resources :users, only: [:create, :show] do
-    resources :discover, only: [:index]
+    resources :discover, only: [:index], controller: "user_discover"
   end
 
   get '/register', to: 'users#new'
