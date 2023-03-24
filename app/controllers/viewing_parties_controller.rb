2ef343b1cc1c810ef4e1eb72041a33ba54c95448
Adam Bailey <ab67319@gmail.com>
2023-03-24 15:21:13 -0600

    refactor: update styling per hound ci sniffs
    Co-authored-by: James Taylor <jrtaylor0614@gmail.com>

--- app/controllers/viewing_parties_controller.rb
+++ app/controllers/viewing_parties_controller.rb
@@ -11,11 +11,11 @@ class ViewingPartiesController < ApplicationController
 
     if viewing_party.save
       flash[:notice] = "Viewing party created"
-      
+
       create_viewing_party_users(params[:users], viewing_party.id, params[:user_id])
 
       redirect_to user_path(params[:user_id])
-      
+
     else
       flash[:notice] = "Unable to create viewing party - #{viewing_party.errors.full_messages}"
       redirect_to new_user_movie_viewing_party_path(params[:user_id], params[:movie_id])
@@ -23,13 +23,14 @@ class ViewingPartiesController < ApplicationController
   end
 
   private
+  
   def viewing_party_params
     params.permit(:duration_minutes, :party_date, :party_time, :movie_id, :host_id)
   end
 
   def create_viewing_party_users(users, viewing_party_id, host_id)
     ViewingPartyUser.create!(user_id: host_id, viewing_party_id: viewing_party_id)
-    
+
     users.reject { |user, checkbox| checkbox == "0"}.each do |user|
       ViewingPartyUser.create!(user_id: user[0].to_i, viewing_party_id: viewing_party_id)
     end
