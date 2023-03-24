5be1aa0170b6796a71114bf66ef00bded7d64042
Adam Bailey <ab67319@gmail.com>
2023-03-23 23:46:14 -0600

    test: create user show page view parties
    Co-authored-by: James Taylor <jrtaylor0614@gmail.com>

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -74,4 +74,5 @@ VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
   config.filter_sensitive_data('<TMDB_API_KEY>') { ENV["TMDB_API_KEY"] }
+  config.configure_rspec_metadata!
 end
